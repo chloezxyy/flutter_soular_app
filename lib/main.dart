10098f39927e982a678e2c1d7f430027f777ea0f
chloe zheng <chloezxy@hotmail.com>
2020-03-22 19:15:45 +0800

    add official login page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: MainPage(),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
