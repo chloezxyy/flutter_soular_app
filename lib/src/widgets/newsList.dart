a0d3ed937382d8a3f7e471a3ec2fb9601069cb47
chloe zheng <chloezxy@hotmail.com>
2020-07-06 00:17:11 +0800

    adjusted font for news section

--- lib/src/widgets/newsList.dart
+++ lib/src/widgets/newsList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_soular_app/src/models/newsArticle.dart';
 import 'package:flutter_soular_app/src/services/webService.dart';
+import 'package:flutter_soular_app/src/theme/color/light_color.dart';
 import 'package:flutter_soular_app/src/utils/constants.dart';
 
 // To display all the news in a ListView control
@@ -22,22 +23,29 @@ class NewsListState extends State<NewsList> {
 
   ListTile _listView(BuildContext context, int index) {
     return ListTile(
-        leading: _newsArticles[index].urlToImage == null
-            ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL)
-            : Image.network(_newsArticles[index].urlToImage),
-        title:
-            Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)));
+        leading: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 45,
+              minHeight: 45,
+              maxWidth: 70 ,
+              maxHeight: 70,
+            ),
+            child: _newsArticles[index].urlToImage == null
+                ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL)
+                : Image.network(_newsArticles[index].urlToImage)),
+        title: Text(_newsArticles[index].title,
+            style: TextStyle(color: LightColor.titleTextColor)));
   }
 
-  ListTile _buildItemsForListView(BuildContext context, int index) {
-    return ListTile(
-      title: _newsArticles[index].urlToImage == null
-          ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL)
-          : Image.network(_newsArticles[index].urlToImage),
-      subtitle:
-          Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)),
-    );
-  }
+  // ListTile _buildItemsForListView(BuildContext context, int index) {
+  //   return ListTile(
+  //     title: _newsArticles[index].urlToImage == null
+  //         ? Image.asset(Constants.NEWS_PLACEHOLDER_IMAGE_ASSET_URL)
+  //         : Image.network(_newsArticles[index].urlToImage),
+  //     subtitle:
+  //         Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)),
+  //   );
+  // }
 
   // @override
   // Widget build(BuildContext context) {
